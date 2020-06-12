8114e1fc8a13654061dba879ca3d50300d98d67b
pagos21 <paga@localhost.yeah>
2020-06-12 16:53:53 +0200

    v 1.1 (WIP on bonus + new features)

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,35 @@
 // Chiedo all'utente il nome
-var fName = prompt("Insert your name here... ")
+var fName = prompt("Insert your name here... ");
 
+// RegEx per controllo spazi o numeri
+
+if (/\s/.test(fName)) {
+    alert("Ci sono degli spazi nel tuo nome")
+    location.reload();
+}
+if (/\d/.test(fName)) {
+    alert("Ci sono numeri nel tuo nome?!?")
+    location.reload();
+
+}
 // Chiedo il cognome
-var sName = prompt("Hi " + fName + ", insert your second name here...")
+var sName = prompt("Hi " + fName + ", insert your second name here...");
+// RegEx per controllo spazi o numeri
+if (/\s/.test(sName)) {
+    alert("Ci sono degli spazi nel tuo cognome")
+    location.reload();
+
+}
+if (/\d/.test(sName)) {
+    alert("Ci sono numeri nel tuo cognome?!?")
+    location.reload();
+
+}
 
 // Chiedo il colore preferito
 
-var fColor = prompt("Perfect! " + fName + " " + sName + " What's your favorite color?")
+var fColor = prompt("Perfect! " + fName + " " + sName + " What's your favorite color?");
 
 // Metto insieme tutto e Stampo la password
 
-document.getElementById('d_pass').innerHTML = fName + sName + fColor + "19"
+document.getElementById('d_pass').innerHTML = fName + sName + fColor + "19";
