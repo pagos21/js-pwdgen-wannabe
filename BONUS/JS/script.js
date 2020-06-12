108d6d3b6cd801f40f8ce57d7a99123a5fb8ee13
pagos21 <paga@localhost.yeah>
2020-06-12 16:56:23 +0200

    v 2.0 (WIP)

--- BONUS/JS/script.js
+++ BONUS/JS/script.js
@@ -1,1 +1,26 @@
- 
+// Caldo benvenuto!
+var today = new Date()
+var curTime = today.getHours()
+
+if (curTime < 12) {
+  document.getElementById('pm_am').innerHTML = "Good morning!"
+} else if (curTime < 18) {
+  document.getElementById('pm_am').innerHTML = "Good afternoon!"
+} else {
+  document.getElementById('pm_am').innerHTML = "Good evening!"
+}
+
+// Rot 13
+
+
+function rot13f() {
+
+
+  alert('Caesar Cipher!!!')
+  var fName = prompt("Insert your name here... ")
+
+  var sName = prompt("Hi " + fName + ", insert your second name here...")
+
+  var fColor = prompt("Perfect! " + fName + " " + sName + " What's your favorite color?")
+
+}
